a0726bc8e474539aa74d5349e0eb677d8b3e8e95
Nupur Agarwal <nupur.agarwal521@gmail.com>
2025-01-11 01:02:57 +0530

    Formil touched method to see the visited Fields

--- src/YoutubeForm.js
+++ src/YoutubeForm.js
@@ -2,33 +2,63 @@ import { useFormik } from "formik"
 
 function YoutubeForm(){
 
-const formik =useFormik({
-    initialValues:{
-        name: '',
-        email: '',
-        channel: ''
-    },
-    onSubmit:values=>{
+const validate = values =>{
+
+    let errors={}
+
+    if(!values.name)
+        errors.name="Required"
+    if(!values.email)
+        errors.email="Required"
+    if(!values.channel)
+         errors.channel="Required"
+
+    console.log(formik.errors)
+    
+    return errors
+}
+
+const initialValues = {
+    name: '',
+    email: '',
+    channel: ''
+}
+
+const onSubmit =values =>{
     console.log(formik.values)
-    }
+}
+
+const formik =useFormik({
+    initialValues,
+    onSubmit,
+    validate
 })
 
-console.log("form values are: ", formik.values)
+console.log("form values are: ", formik.touched)
 return(
     <div>
         <form onSubmit={formik.handleSubmit}>
+            <div style={{marginBottom: '2px'}}>
             <label htmlFor="name">
                Name:  
             </label>
-            <input type="text" name="name" id="name" onChange={formik.handleChange} value={formik.values.name}></input>
-            <label htmlFor="name">
+            <input type="text" name="name" id="name" onChange={formik.handleChange} value={formik.values.name} onBlur={formik.handleBlur}></input>
+             {formik.touched.name ?<div>{formik.errors.name}</div>: null}
+             </div>
+             <div style={{marginBottom: '2px'}}>
+            <label htmlFor="email">
                Email:
             </label>
-            <input type="email" name="email" id="email" onChange={formik.handleChange} value={formik.values.email}></input>
-            <label htmlFor="name">
+            <input type="email" name="email" id="email" onChange={formik.handleChange} value={formik.values.email} onBlur={formik.handleBlur}></input>
+            {formik.touched.email ? <div>{formik.errors.email}</div>: null}
+            </div>
+            <div style={{marginBottom: '2px'}}>
+            <label htmlFor="channel">
                Channel:
             </label>
-            <input type="text" name="channel" id="channel" onChange={formik.handleChange} value={formik.values.channel}></input>
+            <input type="text" name="channel" id="channel" onChange={formik.handleChange} value={formik.values.channel} onBlur={formik.handleBlur}></input>
+            {formik.touched.channel ? <div>{formik.errors.channel}</div>: null}
+            </div>
             <button type="submit">Submit</button>
         </form>
     </div>
